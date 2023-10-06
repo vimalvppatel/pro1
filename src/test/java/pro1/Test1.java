d7e2d9219faf902532ffe4d0bd28a147feb6d672
Vimal Patel <vimalpatel35458@gmail.com>
2023-10-06 19:45:35 +0530

    New Project

--- src/test/java/pro1/Test1.java
+++ src/test/java/pro1/Test1.java
@@ -17,10 +17,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Test1 {
 	public static void main(String[] args) throws InterruptedException {
 
-		WebDriverManager.chromedriver().setup(); 
-		ChromeOptions op = new ChromeOptions();
-		op.addArguments("--remote-allow-origins=*");
-		WebDriver wb = new ChromeDriver(op);
+//		WebDriverManager.chromedriver().setup(); 
+//		ChromeOptions op = new ChromeOptions();
+//		op.addArguments("--remote-allow-origins--*");
+	WebDriver wb = new ChromeDriver();
 		
 		wb.manage().window().maximize();
 
